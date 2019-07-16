14175f0e6f43c5c5a468122fe779abbbd3036979
nghinguyenzero <nghinguyenzero@gmail.com>
2019-07-17 00:09:20 +0700

    53.Hiển thị ds sản phẩm, đổi tên -> products.js

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
 import Product from './Product';
+import { connect } from 'react-redux';
 
 
 class Products extends Component {
     render() {
+        var { products } = this.props;
         return (
             <section className="section">
                 <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
                 <div className="row">
                     {/* <!-- Product --> */}
-                    <Product></Product>
-                    <Product></Product>
-
-                    <Product></Product>
-
+                    {this.showProducts(products)}
                 </div>
             </section>
 
         );
     }
+    
+    showProducts(products){
+        var result = null;
+        if (products.length > 0) {
+            result = products.map((product, index) => {
+                return <Product key={index} product={product}/>
+            });
+        }
+        return result;
+    }
 
 }
-
-export default Products;
+const mapStateToProps = state => { //là một bộ lọc (filter) sử dụng để lấy (select) những thứ trong store mà component yêu cầu
+    return {
+        products: state.products //reducer/index
+    }
+}
+export default connect(mapStateToProps, null)(Products); //The connect() function connects a React component to a Redux store.
--- src/reducers/products.js
+++ src/reducers/products.js
@@ -2,30 +2,33 @@ var inittialState = [
     {
         id: 1,
         name: 'Iphone X',
-        image: '',
+        image: 'http://2tmobile.com.vn/image/cache/imgdata/Sanpham/dien_thoai/Apple/iphone-x-fix/apple-iphone-x-256gb-silver-250x250.jpg',
         description: 'Đây là sản phẩm do Apple sản xuất',
         price: 1000,
         inventory: 10,
+        rating: 4
     },
     {
         id: 2,
-        name: 'Note 10',
-        image: '',
+        name: 'Note 8',
+        image: 'https://img.sam.vn/public/upload/thumbnail/250x250/dien-thoai-samsung-galaxy-note-8-hang-chinh-hang1527734658804.jpeg',
         description: 'Đây là sản phẩm do SamSung sản xuất',
         price: 700,
         inventory: 5,
+        rating: 5
     },
     {
         id: 3,
-        name: 'Experia XZ',
-        image: '',
+        name: 'Xperia XZ',
+        image: 'https://dienmaycholon.vn/public/picture/product/product11282/big/product_11282_1.png',
         description: 'Đây là sản phẩn so Sony sản xuất',
         price: 500,
         inventory: 20,
+        rating: 3
     }
 ];
 
-const product = (state = inittialState, action) => {
+const products = (state = inittialState, action) => {
     switch (action.type) {
         // case value:
         //     break;
@@ -34,4 +37,4 @@ const product = (state = inittialState, action) => {
     }
 }
 
-export default product;
+export default products;
