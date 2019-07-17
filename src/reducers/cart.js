2a6127f6d78db4b80db555d39430b289daee0052
nghinguyenzero <nghinguyenzero@gmail.com>
2019-07-18 01:24:58 +0700

    61 onAddToCart, lưu vào localStorage

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,39 +1,40 @@
-import * as types from './../constants/ActionType';
+import * as Types from './../constants/ActionType';
 var data = JSON.parse(localStorage.getItem('CART'));
-var inittialState = [
-    {
-        product: {
-            id: 1,
-            name: 'Iphone X',
-            image: 'http://2tmobile.com.vn/image/cache/imgdata/Sanpham/dien_thoai/Apple/iphone-x-fix/apple-iphone-x-256gb-silver-250x250.jpg',
-            description: 'Đây là sản phẩm do Apple sản xuất',
-            price: 1000,
-            inventory: 10,
-            rating: 4
-        },
-        quantity: 5
-    },
-    {
-        product:     {
-            id: 3,
-            name: 'Xperia XZ',
-            image: 'https://dienmaycholon.vn/public/picture/product/product11282/big/product_11282_1.png',
-            description: 'Đây là sản phẩn so Sony sản xuất',
-            price: 500,
-            inventory: 20,
-            rating: 3
-        },
-        quantity: 2
-    }
+var inittialState = data ? data:[
 ];
 
 const cart = (state = inittialState, action) => {
+    var { product, quantity } = action;
+    var index = -1;
     switch (action.type) {
-        case types.ADD_TO_CART:
-            console.log(action);
+        case Types.ADD_TO_CART:
+            index = findProductInCart(state, product);
+            if(index!==-1){
+                state[index].quantity +=quantity;
+            } else {
+                state.push({
+                    product,
+                    quantity
+                });
+            }
+            localStorage.setItem('CART',JSON.stringify(state));
             return [...state];
         default: return [...state]; // the spread syntax(cú pháp lây lan) to insert the mid array into the arr array
     }
+}
+
+const findProductInCart = (cart, product) => {
+    var index = -1;
+    var n = cart.length;
+    if (n > 0) {
+        for (let i = 0; i < n; i++){
+            if(cart[i].product.id === product.id) {
+index = i;
+break;
+            }
+        }
+    }
+    return index;
 }
 
 export default cart;
