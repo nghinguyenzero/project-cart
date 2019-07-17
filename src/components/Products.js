a219f802a240deae09d6b558ecbe9e939d08cc6c
nghinguyenzero <nghinguyenzero@gmail.com>
2019-07-17 10:17:41 +0700

    56 ProductsContainer

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,30 +1,17 @@
 import React, { Component } from 'react';
-import Product from './Product';
 
 class Products extends Component {
     render() {
-        var { products } = this.props;
         return (
             <section className="section">
                 <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
                 <div className="row">
                     {/* <!-- Product --> */}
-                    {this.showProducts(products)}
+                    {this.props.children}
                 </div>
             </section>
         );
     }
-
-    showProducts(products){
-        var result = null;
-        if (products.length > 0) {
-            result = products.map((product, index) => {
-                return <Product key={index} product={product}/>
-            });
-        }
-        return result;
-    }
-
 }
 
 export default Products; 
--- src/containers/ProductsContainer.js
+++ src/containers/ProductsContainer.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Products from "./../components/Products";
+import Products from './../components/Products';
+import Product from './../components/Product';
+
 
 
 class ProductsContainer extends Component {
     render() {
         var { products } = this.props;
         return (
-            <Products products={products} />
+            // <Products products={products} />
+            <Products>
+                {this.showProducts(products)} //Truyền vào dạng children
+            </Products>
         );
+    }
+    showProducts(products){
+        var result = null;
+        if (products.length > 0) {
+            result = products.map((product, index) => {
+                return <Product key={index} product={product}/>
+            });
+        }
+        return result;
     }
 }
 
